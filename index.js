45398f651af76586a0229cb3b644326743c56a9f
Tor Brekke Skjøtskift <tor.brekke.skjotskift@gmail.com>
2022-10-25 07:20:59 +0200

    Throw error if file read fails

--- index.js
+++ index.js
@@ -6,22 +6,27 @@ const templateFiles = {};
 export function renderFile(file) {
     const directory = file[0] === '/' ? process.cwd() : getCallerDirectory();
     const filePath = path.join(directory, file);
-    if (!templateFiles[filePath]) {
-        const templateFile = readFileSync(filePath, 'utf8');
-        templateFiles[filePath] = function template(vars) {
-            const keys = Object.keys(vars);
-            // @ts-ignore
-            const values = Object.values(vars);
-            // eslint-disable-next-line no-new-func, prefer-template
-            return new Function(
-                ...keys,
-                `try {
-                    return \`${templateFile}\`;
-                } catch (e) {
-                    throw new Error(\`${filePath} failed. \${e}\`);
-                }`
-            )(...values);
-        };
+    if (templateFiles[filePath]) {
+        return templateFiles[filePath];
     }
+    try {
+        const templateFile = readFileSync(filePath, 'utf8');
+    } catch (e) {
+        throw new Error(`Failed to read ${filepath}`);
+    }
+    templateFiles[filePath] = function template(vars) {
+        const keys = Object.keys(vars);
+        // @ts-ignore
+        const values = Object.values(vars);
+        // eslint-disable-next-line no-new-func, prefer-template
+        return new Function(
+            ...keys,
+            `try {
+                return \`${templateFile}\`;
+            } catch (e) {
+                throw new Error(\`${filePath} failed. \${e}\`);
+            }`
+        )(...values);
+    };
     return templateFiles[filePath];
 }
