fd2f93e11a5f18b167bcde3ef34a2ffe9fd331fe
Tor Brekke Skjøtskift <tor.brekke.skjotskift@gmail.com>
2023-02-10 11:47:03 +0100

    added render test

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ import { getCallerDirectory } from './get-directory.js';
 
 const templateFiles = {};
 export function renderFile(file) {
-    const directory = file[0] === '/' ? process.cwd() : getCallerDirectory();
-    const filePath = path.join(directory, file);
-    if (templateFiles[filePath]) {
-        return templateFiles[filePath];
-    }
-    let templateFile
-    try {
-        templateFile = readFileSync(filePath, 'utf8');
-    } catch (e) {
-        throw new Error(`Failed to read ${filePath}`);
-    }
-    templateFiles[filePath] = function template(vars = {}) {
-        const keys = Object.keys(vars);
-        // @ts-ignore
-        const values = Object.values(vars);
-        // eslint-disable-next-line no-new-func, prefer-template
-        return new Function(
-            ...keys,
-            `try {
-                return \`${templateFile}\`;
-            } catch (e) {
-                throw new Error(\`${filePath} failed. \${e}\`);
-            }`
-        )(...values);
-    };
+  const directory = file[0] === '/' ? process.cwd() : getCallerDirectory();
+  const filePath = path.join(directory, file);
+  if (templateFiles[filePath]) {
     return templateFiles[filePath];
+  }
+  let templateFile;
+  try {
+    templateFile = readFileSync(filePath, 'utf8');
+  } catch (e) {
+    throw new Error(`Failed to read ${filePath}`);
+  }
+  templateFiles[filePath] = function template(vars = {}) {
+    const keys = Object.keys(vars);
+    // @ts-ignore
+    const values = Object.values(vars);
+    // eslint-disable-next-line no-new-func, prefer-template
+    return new Function(
+      ...keys,
+      `try {
+            return \`${templateFile}\`;
+        } catch (e) {
+            throw new Error(\`${filePath} failed. \${e}\`);
+        }`
+    )(...values);
+  };
+  return templateFiles[filePath];
 }
